68d989b8555799340429b251f5b78a3a3b1baa8e
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2021-03-30 21:11:38 +0200

    Add files via upload

--- app.R
+++ app.R
@@ -1,62 +1,121 @@
-
-# Read this shape file with the rgdal library. 
+library(shiny)
 library(leaflet)
-library(rgdal)
-library(maps)
 library(readxl)
-library(dplyr)
+library(rgdal)
 library(sf)
-library(tmap)
+library(data.table) ### Download the tables from a website
 library(tidyverse)
+library(mapview)
 
-world_shape <- st_read("/Users/wemigliari/Documents/R/Rshiny/covid_world/World_Countries__Generalized_.shp")
+###################################################################################################
+### SAO PAULO
+###layers_sp
+metro_sao_paulo <- st_read("/Users/wemigliari/Documents/R/Rshiny/metro_corona_brazil/Munic¡pios_RM_Sao_Paulo.shp")
+class(metro_sao_paulo)
+metro_layer_sp <- st_transform(metro_sao_paulo, "+proj=longlat +datum=WGS84")
 
-world_shape_geo <- st_transform(world_shape, "+proj=longlat +datum=WGS84")
+###Converting multipolygons into two columns_sp
+sf_centers <- metro_layer_sp %>%
+    dplyr::mutate(geometry = st_centroid(geometry))
+sf_centers1 <- sf_centers %>% mutate(lat = unlist(map(sf_centers$geometry,2)), long = unlist(map(sf_centers$geometry,1)))
 
-world_shape_geo1 <- world_shape_geo %>%
-    dplyr::mutate(geometry = st_centroid(world_shape_geo$geometry))
+##################################################################################################
 
-### Use tidyverse to pass mutate argument. 
-world_shape_geo2 <- world_shape_geo1%>%  
-    mutate(lat = unlist(map(world_shape_geo1$geometry,2)), long = unlist(map(world_shape_geo1$geometry,1)))
+### Data on coronavirus
+brazil_corona <- read_excel("corona_brazil.xlsx")
+brazil_corona <- mutate_all(brazil_corona, funs(toupper))
+NM_MUNICIP <- brazil_corona$municipio
+brazil_corona1 <- cbind(brazil_corona, NM_MUNICIP)
 
-####Data on Corona19
-covid_data <- read_excel("/Users/wemigliari/Documents/R/tabelas/covid_world.xlsx")
-COUNTRYAFF<- covid_data$location
-location1 <- cbind(covid_data, COUNTRYAFF)
+##################################################################################################
 
+### Merging the shapefiles with the data_sp
+sf_centers2 <- merge(sf_centers1, brazil_corona1, by= "NM_MUNICIP", all=T)
+rownames(sf_centers2) <- sf_centers2$Row.names; sf_centers2$Row.names <- NULL
+sf_centers2
 
-### Merging the two tables
+##################################################################################################
 
-world_covid <- merge(world_shape_geo2, location1, by= "COUNTRYAFF", all=T)
-rownames(world_covid) <- world_covid$Row.names; world_covid$Row.names <- NULL
-summary(world_covid)
-
-qpal <- colorQuantile("Blues", covid_data$population, n = 9)
-
-# Prepare the text for tooltips:
-mytext <- paste(
-    "Country: ", world_covid$COUNTRYAFF,"<br/>", 
-    "Population: ",round(world_covid$population, 2),"<br/>", 
-    "Total Cases (per million): ", world_covid$total_cases_per_million, "<br/>",
-    "Total Deaths (per million): ", world_covid$total_deaths_per_million,"<br/>",
-    "Aged 70 Older (% Population): ", world_covid$aged_70_older,"<br/>", 
+###Labels_sp
+tocan <- makeAwesomeIcon(icon = 'flag', markerColor = 'red', library='fa', iconColor = 'black')
+deaths_sp <- paste(
+    "Municipality: ",sf_centers2$NM_MUNICIP, "<br/>",
+    "Fatal Cases: ",sf_centers2$obitosAcumulado, "<br/>",
+    "Total Cases: ",sf_centers2$casosAcumulado, "<br/>",
     sep="") %>%
     lapply(htmltools::HTML)
 
-```{r, echo = FALSE, comment = FALSE, warning = FALSE}
+###Map_sp
 
-# Final Map
-
-leaflet(world_covid) %>% addTiles()%>% 
-    addProviderTiles(providers$CartoDB)%>%
-    setView( lat=10, lng=0 , zoom=2) %>%
-    addCircleMarkers(lng = world_covid$long, lat = world_covid$lat, weight = 1, 
+sao_paulo <- leaflet(sf_centers2) %>% addTiles() %>%
+    addProviderTiles(providers$Stamen.Toner)%>%
+    setView(-46.625290, -23.533773, zoom = 9)%>%
+    addPolygons(data = metro_layer_sp, color = "gold")%>%
+    addCircleMarkers(lng = sf_centers2$long, lat = sf_centers2$lat, weight = 1, 
                      radius = 5,
-                     label = mytext,
-                     color = ~qpal(population),
-                     fillOpacity = 0.3)%>%
-    addLegend("bottomright", pal = qpal, values = ~population, 
-              title = "COVID-19 Scale (%)", opacity = 1)
-```
+                     label = deaths_sp,
+                     color = "purple",
+                     fillOpacity = 0.6)%>%
+    addAwesomeMarkers(-48.32766, -10.16745, label = "Berg e Cíntia, fiquem em casa!", icon = tocan)
+
+sao_paulo
+
+##################################################################################################
+
+### RIO DE JANEIRO
+
+###layers_rj
+metro_rj <- st_read("/Users/wemigliari/Documents/R/Rshiny/metro_corona_brazil/Munic¡pios_RM_Rio_de_Janeiro.shp")
+class(metro_rj)
+metro_layer_rj <- st_transform(metro_rj, "+proj=longlat +datum=WGS84")
+
+###Converting multipolygons into two columns_rj
+sf_centers_rj <- metro_layer_rj %>%
+    dplyr::mutate(geometry = st_centroid(geometry))
+sf_centers_rj1 <- sf_centers_rj %>% mutate(lat = unlist(map(sf_centers_rj$geometry,2)), long = unlist(map(sf_centers_rj$geometry,1)))
+
+##################################################################################################
+
+### Data on coronavirus
+brazil_corona <- read_excel("corona_brazil.xlsx")
+brazil_corona <- mutate_all(brazil_corona, funs(toupper))
+NM_MUNICIP <- brazil_corona$municipio
+brazil_corona1 <- cbind(brazil_corona, NM_MUNICIP)
+
+##################################################################################################
+
+### Merging the shapefiles with the data_rj
+sf_centers_rj2 <- merge(sf_centers_rj1, brazil_corona1, by= "NM_MUNICIP", all=T)
+rownames(sf_centers_rj2) <- sf_centers_rj2$Row.names; sf_centers_rj2$Row.names <- NULL
+sf_centers_rj2
+
+##################################################################################################
+
+###Labels_rj
+
+deaths_rj <- paste(
+    "Municipality: ",sf_centers_rj2$NM_MUNICIP, "<br/>",
+    "Fatal Cases: ",sf_centers_rj2$obitosAcumulado, "<br/>",
+    "Total Cases: ",sf_centers_rj2$casosAcumulado, "<br/>",
+    sep="") %>%
+    lapply(htmltools::HTML)
+
+###Map_rj
+
+rio_de_janeiro <- leaflet(sf_centers_rj2) %>% addTiles() %>%
+    addProviderTiles(providers$Stamen.Toner)%>%
+    setView(-43.196388, -22.908333, zoom = 9)%>%
+    addPolygons(data = metro_layer_rj, color = "gold")%>%
+    addCircleMarkers(lng = sf_centers_rj2$long, lat = sf_centers_rj2$lat, weight = 1, 
+                     radius = 5,
+                     label = deaths_rj,
+                     color = "purple",
+                     fillOpacity = 0.6)
+
+rio_de_janeiro
+
+##################################################################################################
+
+sync(sao_paulo, rio_de_janeiro)
+
 
